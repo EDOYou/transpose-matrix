d8aff5d7100f307aaa6a31454b1aef46c219e205
Taghiyev <ktaghiyev01@gmail.com>
2022-12-05 00:41:25 +0100

    transpose-matrix

--- src/main/java/com/epam/rd/autotasks/matrices/TransposeMatrix.java
+++ src/main/java/com/epam/rd/autotasks/matrices/TransposeMatrix.java
@@ -1,23 +1,29 @@
+//**************************************************** Transpose-Matrix ****************************************************************
 package com.epam.rd.autotasks.matrices;
 import java.util.Arrays;
 
 public class TransposeMatrix {
     public static int[][] transpose(int[][] matrix) {
 
-        //Put your code here
+        int[][] transpose = new int[matrix[0].length][matrix.length];
 
-        return null;
+        for (int i = 0; i < matrix.length; i++) {
+            for (int j = 0; j < matrix[0].length; j++) {
+
+                transpose[j][i] = matrix[i][j];
+
+            }
+        }
+
+        return transpose;
     }
 
     public static void main(String[] args) {
 
-        System.out.println("Test your code here!\n");
-
         // Get a result of your code
 
-        int[][] matrix = {
-                {1, 2},
-                {7, -13} };
+          int[][] matrix = { {-4, -65, 56},
+                             {78, -13, 32} };
 
         int[][] result = transpose(matrix);
         System.out.println(Arrays.deepToString(result).replace("],", "]\n"));
